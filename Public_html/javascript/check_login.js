39696c00aedbf72935d75392d088fe5b737e194a
sscaglio <blackpaper1993@gmail.com>
2016-04-07 00:23:24 +0200

    Update check_login.js
    aggiunto .value variabili username e password
    testato script con firefox e sembra che tutto funzioni

--- Public_html/javascript/check_login.js
+++ Public_html/javascript/check_login.js
@@ -1,14 +1,17 @@
 function check_login() {
 
-var username = document.getElementById("username");
-var password = document.getElementById("password");
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
 
 if (username == "" || username == "undefined") {
 	alert("USERNAME non corretto o vuoto");
 	document.getElementById("username").focus();	
 	}
 
+if (password == "" || password == "undefined") {
+	alert("PASSWORD non corretta o vuota")
+	document.getElementById("password").focus();
 }
-if (password) {
 
+return false;
 }
